1fa5bd75d81f6efbe947141cfec08d5fc7d1fb67
Daoud Clarke <dclarke@luc.id>
2021-10-30 23:07:10 +0100

    Get URLs and filter on language

--- index.py
+++ index.py
@@ -1,25 +1,45 @@
 import re
 
-from pyspark.sql.types import StructType, StructField, StringType
+import mmh3
+from pyspark.sql.types import StructType, StructField, StringType, LongType
 
 from sparkcc import CCSparkJob
+
+
+NUM_CHARS = 1000
+NUM_PAGES = 1024
 
 
 class Indexer(CCSparkJob):
     word_pattern = re.compile(r'\w+', re.UNICODE)
 
-    output_schema = StringType()
+    output_schema = StructType([
+        StructField("term_hash", LongType(), True),
+        StructField("uri", StringType(), True),
+        StructField("extract", StringType(), True),
+    ])
 
     def process_record(self, record):
         if not self.is_wet_text_record(record):
             return
-        data = record.content_stream().read().decode('utf-8')
-        yield data[:1000]
 
-        # words = map(lambda w: w.lower(),
-        #             Indexer.word_pattern.findall(data))
-        # for word in words:
-        #     yield word, data[:100]
+        # print("Process", record.format, record.rec_type, record.rec_headers, record.raw_stream,
+        #       record.http_headers, record.content_type, record.length)
+
+        language = record.rec_headers.get_header('WARC-Identified-Content-Language')
+        if language != 'eng':
+            return
+
+        uri = record.rec_headers.get_header('WARC-Target-URI')
+        extract = record.content_stream().read().decode('utf-8')[:NUM_CHARS]
+
+        words = map(lambda w: w.lower(),
+                    Indexer.word_pattern.findall(extract))
+        for word in words:
+            key_hash = mmh3.hash(word, signed=False)
+            key = key_hash % NUM_PAGES
+
+            yield key, uri, extract
 
     def run_job(self, sc, sqlc):
         input_data = sc.textFile(self.args.input,
