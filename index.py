fac39b321f39a121f1bfd397bbca5a3639c4409c
Daoud Clarke <dclarke@luc.id>
2021-11-01 22:16:26 +0000

    Extract title separately

--- index.py
+++ index.py
@@ -3,15 +3,18 @@ import re
 import mmh3
 import spacy as spacy
 from pyspark.sql.types import StructType, StructField, StringType, LongType
+from spacy.tokens import Doc, Token, Span
 
 from sparkcc import CCSparkJob
 
 
-NUM_CHARS = 1000
+NUM_CHARS_TO_ANALYSE = 1000
+NUM_TITLE_CHARS = 65
+NUM_EXTRACT_CHARS = 155
 NUM_PAGES = 1024
 
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm", disable=['lemmatizer', 'ner'])
 
 
 # def tokenizer(sentence):
@@ -21,11 +24,28 @@ nlp = spacy.load("en_core_web_sm")
 #             and not token.is_space]
 
 
+def is_valid_token(token: Token):
+    return not token.is_punct and not token.is_space and not token.is_stop
+
+
+def extract_terms(span: Span, terms: set[str], max_chars: int) -> str:
+    extract = None
+    for token in span:
+        text = span[:token.idx + 1].text
+        if len(text) > max_chars - 1:
+            return extract + '…' if extract is not None else None
+        extract = text
+        if is_valid_token(token):
+            terms.add(str(token).lower())
+    return extract
+
+
 class Indexer(CCSparkJob):
     output_schema = StructType([
         StructField("term_hash", LongType(), False),
         StructField("term", StringType(), False),
         StructField("uri", StringType(), False),
+        StructField("title", StringType(), False),
         StructField("extract", StringType(), False),
     ])
 
@@ -41,18 +61,28 @@ class Indexer(CCSparkJob):
             return
 
         uri = record.rec_headers.get_header('WARC-Target-URI')
-        extract = record.content_stream().read().decode('utf-8')[:NUM_CHARS]
+        content = record.content_stream().read().decode('utf-8')[:NUM_CHARS_TO_ANALYSE]
 
-        tokens = nlp.tokenizer(extract)
+        doc = nlp(content)
+        sentences = list(doc.sents)
+        title_span = sentences[0]
 
-        for token in tokens[:-1]:
-            if token.is_punct or token.is_space or token.is_stop:
-                continue
+        terms = set()
+        title = extract_terms(title_span, terms, NUM_TITLE_CHARS)
 
-            term = str(token).lower()
+        if not title:
+            return
+
+        extract_start_index = title_span.end
+        extract = extract_terms(doc[extract_start_index:], terms, NUM_EXTRACT_CHARS)
+
+        if not extract:
+            return
+
+        for term in terms:
             key_hash = mmh3.hash(term, signed=False)
             key = key_hash % NUM_PAGES
-            yield key, term, uri, extract
+            yield key, term, uri, title, extract
 
     def run_job(self, sc, sqlc):
         input_data = sc.textFile(self.args.input,
