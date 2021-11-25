8fa418665eacfb2ae8fe733f5a795e64ef62e159
Daoud Clarke <daoud.clarke@gmail.com>
2021-11-25 22:27:03 +0000

    Try and fix bootstrap script using python3 and sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
+#!/bin/bash -xe
 
-pip install boto3==1.19.7 botocore==1.22.7 jusText==3.0.0 langdetect==1.0.9 \
+sudo python3 -m pip install boto3==1.19.7 botocore==1.22.7 jusText==3.0.0 langdetect==1.0.9 \
     lxml==4.6.3 numpy==1.21.3 pandas==1.3.4 pyarrow==6.0.0 spacy=2.3.5 \
     warcio==1.7.4 zstandard==0.16.0
 
-python -m spacy download en_core_web_sm
+sudo python3 -m spacy download en_core_web_sm
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+aws s3 cp sparkcc.py s3://tinysearch/code/
+aws s3 cp extract.py s3://tinysearch/code/
+aws s3 cp bootstrap.sh s3://tinysearch/code/
+
 aws emr create-cluster \
     --applications Name=Spark Name=Zeppelin \
     --ec2-attributes '{"InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-9a95acdc","EmrManagedSlaveSecurityGroup":"sg-050f146bb97eaa250","EmrManagedMasterSecurityGroup":"sg-054778695d46ae657"}' \
@@ -5,9 +9,10 @@ aws emr create-cluster \
     --enable-debugging \
     --release-label emr-5.33.1 \
     --log-uri 's3n://tinysearch/pyspark-logs/' \
-    --bootstrap-actions '{"Path": "s3://tinysearch/code/bootstrap.sh"}'
-    --steps '[{"Args":["spark-submit","--deploy-mode","cluster","s3://tinysearch/code/extract.py"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Spark application"}]' \
+    --bootstrap-actions '{"Path": "s3://tinysearch/code/bootstrap.sh"}' \
+    --steps '[{"Args":["spark-submit","--deploy-mode","cluster","s3://tinysearch/code/extract.py","--py-files","s3://tinysearch/code/sparkcc.py"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Spark application"}]' \
     --name 'TinySearch' \
     --instance-groups '[{"InstanceCount":2,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"CORE","InstanceType":"m4.large","Name":"Core Instance Group"},{"InstanceCount":1,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"MASTER","InstanceType":"m4.large","Name":"Master Instance Group"}]' \
     --configurations '[{"Classification":"spark","Properties":{}}]' \
-    --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-west-2
+    --scale-down-behavior TERMINATE_AT_TASK_COMPLETION --region us-east-1 \
+    --auto-terminate
