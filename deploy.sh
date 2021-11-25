b1f92719edaba7b021c5f4a021de9e36db71709b
Daoud Clarke <daoud.clarke@gmail.com>
2021-11-25 22:00:39 +0000

    Add bootstrap script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ aws emr create-cluster \
     --enable-debugging \
     --release-label emr-5.33.1 \
     --log-uri 's3n://tinysearch/pyspark-logs/' \
+    --bootstrap-actions '{"Path": "s3://tinysearch/code/bootstrap.sh"}'
     --steps '[{"Args":["spark-submit","--deploy-mode","cluster","s3://tinysearch/code/extract.py"],"Type":"CUSTOM_JAR","ActionOnFailure":"CONTINUE","Jar":"command-runner.jar","Properties":"","Name":"Spark application"}]' \
     --name 'TinySearch' \
     --instance-groups '[{"InstanceCount":2,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"CORE","InstanceType":"m4.large","Name":"Core Instance Group"},{"InstanceCount":1,"EbsConfiguration":{"EbsBlockDeviceConfigs":[{"VolumeSpecification":{"SizeInGB":32,"VolumeType":"gp2"},"VolumesPerInstance":1}]},"InstanceGroupType":"MASTER","InstanceType":"m4.large","Name":"Master Instance Group"}]' \
